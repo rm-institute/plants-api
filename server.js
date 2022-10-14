8001c51e0e65bd4f08bd93b6c9d0938db06dc5bb
Rina Maar <rina.maar@hotmail.com>
2022-10-14 17:11:14 +0100

    Added Heroku PORT

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.get("/api/:name", (request, response) => {
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is running on ${PORT}.`)
 })
